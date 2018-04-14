ce4b09a8469c6f8d91a7933b056bfcf78b1d527b
Martin Akman <martin@akm.com.au>
2018-04-14 13:30:55 +1000

    Improve email templates

--- templates/widgets/bank_details_email.html.php
+++ templates/widgets/bank_details_email.html.php
@@ -1,23 +1,23 @@
-<table style="background-color: #FFF;width: 100%;padding: 20px; max-width: 500px;">
+<table style="background-color: #FFF;width: 100%;">
 <thead>
 <tr>
 <th colspan="2" style="text-align: left;"><?php echo $text_direct_deposit; ?></th>
 </tr>
 </thead>
 <tr>
-<th style="text-align: right;border-top: 1px solid #ddd;"><?php echo $text_bank; ?>: </th>
-<td style="border-top: 1px solid #ddd;"><?php echo $module_config->bank; ?></td>
+<th><?php echo $text_bank; ?>: </th>
+<td><?php echo $module_config->bank; ?></td>
 </tr>
 <tr>
-<th style="text-align: right;border-top: 1px solid #ddd;"><?php echo $text_bsb; ?>: </th>
-<td style="border-top: 1px solid #ddd;"><?php echo $module_config->bsb; ?></td>
+<th><?php echo $text_bsb; ?>: </th>
+<td><?php echo $module_config->bsb; ?></td>
 </tr>
 <tr>
-<th style="text-align: right;border-top: 1px solid #ddd;"><?php echo $text_account_number; ?>: </th>
-<td style="border-top: 1px solid #ddd;"><?php echo $module_config->account_number; ?></td>
+<th><?php echo $text_account_number; ?>: </th>
+<td><?php echo $module_config->account_number; ?></td>
 </tr>
 <tr>
-<th style="text-align: right;border-top: 1px solid #ddd;"><?php echo $text_deposit_description; ?>: </th>
-<td style="border-top: 1px solid #ddd;"><?php echo $deposit_description;  ?></td>
+<th><?php echo $text_deposit_description; ?>: </th>
+<td><?php echo $deposit_description;  ?></td>
 </tr>
 </table>
